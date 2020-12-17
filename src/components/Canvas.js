7cfcef57b1286858aecad4acb79fa65fa7b6e6d3
DrLiamKless <67854560+DrLiamKless@users.noreply.github.com>
2020-12-17 14:05:57 +0200

    can download

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,23 +1,51 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { fabric } from 'fabric';
 import { Button } from 'react-bootstrap';
+import { saveAs } from 'file-saver';
 
 export default function Canvas({ image }) {
   const [canvas, setCanvas] = useState(undefined);
+  const canvasRef = useRef(undefined)
   const [decorations, setDecorations] = useState([])
   
+  // canvas initiator 
+  const initCanvas = () => (
+    new fabric.Canvas('canvas', {
+        height: image?.height,
+        width: image?.width,
+        // backgroundColor: null,
+        // backgroundImage: image?.url,
+        // foreignObjectRendering: true,
+        allowTaint: true,
+      })
+  )
+  
+  // create canvas
+  useEffect(() => {
+    console.log(
+      canvasRef
+      )
+  }, [canvasRef]);
+
   useEffect(() => {
     setCanvas(initCanvas());
   }, []);
-  
-  const initCanvas = () => (
-  new fabric.Canvas('canvas', {
-      height: image?.height,
-      width: image?.width,
-      backgroundImage: image?.url
-    })
-  )
 
+  //  setDecorations on start
+  useEffect(() => {
+    if(canvas) {
+      setTimeout(() => {
+        addText(canvas, 'topText', image.height / 6, image.width / 3, image.width /8);
+        addText(canvas, 'bottomText', image.height / 1.5, image.width / 4, image.width /8);
+        addBackgroundImage(canvas, image)
+      }, 500)
+    }
+  }, [canvas]);
+  
+  // join elements to decorations
+  const joinToDecorations = ((decoration) => {setDecorations(prevDecorations => [...prevDecorations, decoration])})
+  
+  // add rectangle
   const addRect = canvi => {
     const rect = new fabric.Rect({
       height: 280,
@@ -28,20 +56,36 @@ export default function Canvas({ image }) {
     canvi.renderAll();
   }
 
-  const joinToDecorations = ((decoration) => {setDecorations(prevDecorations => [...prevDecorations, decoration])})
-
-  const addText = (canvi, text) => {
+  // add text
+  const addText = (canvi, text, top=0, left=0,fontSize=100) => {
     const textBox = new fabric.Textbox(text);
+    textBox.set({
+      top,
+      left,
+      fontSize,
+    });
     canvi.add(textBox);
-    canvi.renderAll();
+    // canvi.renderAll();
     joinToDecorations(textBox)
+  }
+
+  const addBackgroundImage = (canvi, image) => {
+      new fabric.Image.fromURL(image.url, (img => {
+      canvi.backgroundImage = img;
+      canvi.renderAll()
+    }) ,{crossOrigin: "anonymous"});
+  }
+
+  const downloadMeme = (canvi, name="myMeme.jpg") => {
+    saveAs(canvi.toDataURL("image/png").replace("image/png", "image/octet-stream"), name)
   }
 
     return(
       <div>
         <h1>Editor</h1>
         <Button onClick={() => addText(canvas, 'add')}>Add Text</Button>
-        <canvas id="canvas" />
+        <Button onClick={() => {downloadMeme(canvas)}}>download</Button>
+        <canvas id="canvas" ref={canvasRef} />
       </div>
   );
 }
